aa40339dc9ffa2c27f7e44a05aefa1ed9f7a310b
Eric <ericl16384@gmail.com>
2023-08-20 16:11:30 -0700

    added basic interception

--- interception.py
+++ interception.py
@@ -1,5 +1,10 @@
-import pygame
-import numpy as np
+import numpy as np, pygame
+
+def to_vector(v):
+    return np.array(v, dtype=np.float32)
+
+def vector_to_int_tuple(vector):
+    return tuple(np.array(vector, dtype=int))
 
 def magnitude(vector):
     return np.linalg.norm(vector)
@@ -14,41 +19,70 @@ def find_intercept_vector(launcher_position, launcher_velocity, target_position,
 
     displacement = target_position - launcher_position
 
-    vector = normalize(vector) * projectile_speed
+    vector = normalize(displacement) * projectile_speed
 
     return vector
 
 
 class Projectile:
+    radius = 5
+
     def __init__(self, pos, vel) -> None:
-        self.pos = np.array(pos)
-        self.vel = np.array(vel)
+        self.pos = to_vector(pos)
+        self.vel = to_vector(vel)
+
+        self.valid = True
     
     def draw(self, surface):
-        pygame.draw.circle(surface, "yellow", np.array(self.pos), 5)
+        pygame.draw.circle(surface, "yellow", vector_to_int_tuple(self.pos), self.radius)
+    
+    def update(self, objects):
+        self.pos += self.vel
+
+        for o in objects:
+            if isinstance(o, Target) and magnitude(o.pos - self.pos) <= self.radius + o.radius:
+                self.valid = False
 
 class Shooter:
-    shoot_speed = 1
+    radius = 15
+
+    projectile_speed = 4
     shoot_reload = 60
 
     def __init__(self, pos) -> None:
-        self.pos = np.array(pos)
+        self.pos = to_vector(pos)
+
+        self.reload_countdown = 0
+
+        self.valid = True
     
     def draw(self, surface):
-        pygame.draw.circle(surface, "blue", np.array(self.pos), 15)
+        pygame.draw.circle(surface, "blue", vector_to_int_tuple(self.pos), self.radius)
     
     def update(self, objects):
-        pass
+        if not self.reload_countdown:
+            for o in objects:
+                if isinstance(o, Target):
+                    self.shoot_at_target(objects, o)
+                    break
 
-    def get_shot(self, target):
-        return Projectile(self.pos, find_intercept_vector(
-            self.pos, np.array((0, 0)), target.pos, target.vel
-        ))
+    def shoot_at_target(self, objects, target):
+        self.reload_countdown = self.shoot_reload
+        objects.append(Projectile(self.pos, find_intercept_vector(
+            self.pos, to_vector((0, 0)), target.pos, target.vel, self.projectile_speed
+        )))
 
 class Target:
+    radius = 10
+
     def __init__(self, pos, vel) -> None:
-        self.pos = np.array(pos)
-        self.vel = np.array(vel)
+        self.pos = to_vector(pos)
+        self.vel = to_vector(vel)
+
+        self.valid = True
     
     def draw(self, surface):
-        pygame.draw.circle(surface, "red", np.array(self.pos), 10)
+        pygame.draw.circle(surface, "red", vector_to_int_tuple(self.pos), self.radius)
+    
+    def update(self, objects):
+        self.pos += self.vel
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import random, pygame
 
 import interception
 
@@ -27,76 +27,23 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
-    # Clear the screen and set the screen background
-    screen.fill("white")
 
+    screen.fill("black")
 
     for o in objects:
         o.draw(screen)
 
-
-
-    # # Draw on the screen a green line from (0, 0) to (50, 30)
-    # # 5 pixels wide. Uses (r, g, b) color - medium sea green.
-    # pygame.draw.line(screen, (60, 179, 113), [0, 0], [50, 30], 5)
-
-    # # Draw on the screen a green line from (0, 50) to (50, 80)
-    # # Because it is an antialiased line, it is 1 pixel wide.
-    # # Uses (r, g, b) color - medium sea green.
-    # pygame.draw.aaline(screen, (60, 179, 113), [0, 50], [50, 80], True)
-
-    # # Draw on the screen 3 black lines, each 5 pixels wide.
-    # # The 'False' means the first and last points are not connected.
-    # pygame.draw.lines(
-    #     screen, "black", False, [[0, 80], [50, 90], [200, 80], [220, 30]], 5
-    # )
-
-    # # Draw a rectangle outline
-    # pygame.draw.rect(screen, "black", [75, 10, 50, 20], 2)
-
-    # # Draw a solid rectangle. Same color as "black" above, specified in a new way
-    # pygame.draw.rect(screen, (0, 0, 0), [150, 10, 50, 20])
-
-    # # Draw a rectangle with rounded corners
-    # pygame.draw.rect(screen, "green", [115, 210, 70, 40], 10, border_radius=15)
-    # pygame.draw.rect(
-    #     screen,
-    #     "red",
-    #     [135, 260, 50, 30],
-    #     0,
-    #     border_radius=10,
-    #     border_top_left_radius=0,
-    #     border_bottom_right_radius=15,
-    # )
-
-    # # Draw an ellipse outline, using a rectangle as the outside boundaries
-    # pygame.draw.ellipse(screen, "red", [225, 10, 50, 20], 2)
-
-    # # Draw an solid ellipse, using a rectangle as the outside boundaries
-    # pygame.draw.ellipse(screen, "red", [300, 10, 50, 20])
-
-    # # This draws a triangle using the polygon command
-    # pygame.draw.polygon(screen, "black", [[100, 100], [0, 200], [200, 200]], 5)
-
-    # # Draw an arc as part of an ellipse.
-    # # Use radians to determine what angle to draw.
-    # pygame.draw.arc(screen, "black", [210, 75, 150, 125], 0, pi / 2, 2)
-    # pygame.draw.arc(screen, "green", [210, 75, 150, 125], pi / 2, pi, 2)
-    # pygame.draw.arc(screen, "blue", [210, 75, 150, 125], pi, 3 * pi / 2, 2)
-    # pygame.draw.arc(screen, "red", [210, 75, 150, 125], 3 * pi / 2, 2 * pi, 2)
-
-    # # Draw a circle
-    # pygame.draw.circle(screen, "blue", [60, 250], 40)
-
-    # # Draw only one circle quadrant
-    # pygame.draw.circle(screen, "blue", [250, 250], 40, 0, draw_top_right=True)
-    # pygame.draw.circle(screen, "red", [250, 250], 40, 30, draw_top_left=True)
-    # pygame.draw.circle(screen, "green", [250, 250], 40, 20, draw_bottom_left=True)
-    # pygame.draw.circle(screen, "black", [250, 250], 40, 10, draw_bottom_right=True)
-
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
     pygame.display.flip()
 
-# Be IDLE friendly
+
+    i = 0
+    while i < len(objects):
+        o = objects[i]
+        o.update(objects)
+        if not o.valid:
+            objects.pop(i)
+            continue
+        i += 1
+
+
 pygame.quit()
